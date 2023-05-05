a00840be09d88bbe8e5035fe364fd98b9990d541
Felix Roberge <roberge.felix@gmail.com>
2023-05-05 12:17:55 -0400

    add parameter to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-3scale import openapi -d https://a379941059dd61bfd1f1b0b50ba9ca5f@3scale-admin.apps.home.myocp.net openapi.json
+3scale import openapi -d https://a379941059dd61bfd1f1b0b50ba9ca5f@3scale-admin.apps.home.myocp.net $1
