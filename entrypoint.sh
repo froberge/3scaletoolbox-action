e645d44f6270df50982407c29bf6b18618c170e3
Felix Roberge <roberge.felix@gmail.com>
2023-05-08 12:40:08 -0400

    fix the if then

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,18 @@ echo "Upldoate the API into 3scale"
 executeLine="https://"+$1+"@"+ $2 $3
 
 if [ ! -z "$4" ]
-else 
+then 
     executeline+=" --default-credentials-userkey " +  $4
 fi
 
 if [ ! -z "$5" ]
-else
+then
     executeline+="--override-private-base-url=" + $5
 fi
 
 
 if [ ! -z "$6" ]
-else
+then
     executeline+="--override-public-basepath=" + $6
 fi
 
