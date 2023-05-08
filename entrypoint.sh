8bd6da719c0184edef5e0ff6d2da135155d635a2
Felix Roberge <roberge.felix@gmail.com>
2023-05-08 12:28:34 -0400

    add parameters to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,27 @@
 
 echo "Upldoate the API into 3scale"
 
-if [ -z "$4" ]
-then
-    3scale import openapi -d https://$1@$2 $3 
+executeLine="https://"+$1+"@"+ $2 $3
+
+if [ ! -z "$4" ]
 else 
-    3scale import openapi -d https://$1@$2 $3 --default-credentials-userkey $4
+    executeline+=" --default-credentials-userkey " +  $4
 fi
+
+if [ ! -z "$5" ]
+else
+    executeline+="--override-private-base-url=" + $5
+fi
+
+
+if [ ! -z "$6" ]
+else
+    executeline+="--override-public-basepath=" + $6
+fi
+
+echo $executeParam
+
+3scale import openapi -d $executeParam
+
+
+
